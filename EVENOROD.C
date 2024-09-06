bbfcacb2d721d3ea58752d3dd5c6ac3ffda6cb77
Dhanushiya-j <dhanushiyajershilin.2006@gmail.com>
2024-09-06 11:45:40 +0530

    Add files via upload

---
+++ EVENOROD.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int no1;
+clrscr();
+printf("enter no1");
+scanf("%d\n",no1);
+if(no1/2==0)
+printf("the number is even");
+else
+printf("the number id odd");
+getch();
+}
